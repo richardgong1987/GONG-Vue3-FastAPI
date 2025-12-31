1d63d3f7a0c99e7a23fce442569192f47a4a3859
richardgong1987 <richardgong1988@gmail.com>
2025-12-31 21:07:55 +0900

    updated

--- ruoyi-fastapi-backend/server.deploy.sh
+++ ruoyi-fastapi-backend/server.deploy.sh
@@ -1,4 +1,4 @@
- git push -u origin main -f
+ git push -u origin master -f
 
 ssh root@100.64.0.6 'bash -s' <<EOF
 set -e
@@ -13,7 +13,7 @@ cd ruoyi-fastapi-backend
 sh build.sh
 
 
-echo "♻️ Restarting server1,server2..."
+echo "♻️ Restarting gong-fastapi-backend-1"
 docker compose down -v
 docker compose up -d --force-recreate
 
